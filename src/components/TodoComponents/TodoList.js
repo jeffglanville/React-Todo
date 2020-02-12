b8dec758ec4d72510ea5b73e5261637ecb5398f0
Jeff Glanville <jeff.glanville@outlook.com>
2020-02-11 18:14:44 -0600

    add data to the Todo List

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,20 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React, { Component } from 'react'
+
+class TodoList extends Component {
+    constructor(){
+        super();
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h3>Add Data Here</h3>
+            </div>
+        )
+    }
+}
+
+
+export default TodoList;
